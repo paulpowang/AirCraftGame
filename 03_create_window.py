29161027e136ab52ac88dc2f75e91c3f014b1cf1
PoHung Wang <wang.po.hung@gmail.com>
2019-02-09 14:36:10 -0800

    update frame rate with dragon motion

--- 03_create_window.py
+++ 03_create_window.py
@@ -22,8 +22,30 @@ pygame.display.update()
 
 
 
+# Create Game clock
+clock = pygame.time.Clock()
+
+
+# 1. Define Rect for dragon init position
+dragon_rect = pygame.Rect(150, 500, 50, 43)
+
 # Game Loop
+i = 0
 while True:
-    pass
+
+    clock.tick(60)  # set the frame rate 60 per second
+
+    # 2. Change dragon position
+    dragon_rect.y -= 1
+
+    # 3. use blit to draw
+    screen.blit(bg, (0, 0))
+    screen.blit(dragon, dragon_rect)
+
+    # 4. update frame
+    pygame.display.update()
+
+    if dragon_rect.y <= (0 - dragon_rect.height):
+        dragon_rect.y = 720
 
 pygame.quit()
