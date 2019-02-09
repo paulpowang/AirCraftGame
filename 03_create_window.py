4244072dfee24d5a17c7b8265ebf68cb7ef56f93
PoHung Wang <wang.po.hung@gmail.com>
2019-02-09 13:46:05 -0800

    add dragon

--- 03_create_window.py
+++ 03_create_window.py
@@ -5,6 +5,23 @@ pygame.init()
 # Create Window
 screen = pygame.display.set_mode((437, 720))
 
+# draw background
+# 1. upload image
+bg = pygame.image.load("./image/background.png")
+screen.blit(bg, (0, 0))
+
+# pygame.display.update()
+
+
+# draw dragon
+dragon = pygame.image.load("./image/dragon.png")
+screen.blit(dragon, (150, 500))
+
+# update screen
+pygame.display.update()
+
+
+
 # Game Loop
 while True:
     pass
